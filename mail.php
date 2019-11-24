1e0c372dc4c5bcdea437eff00f0db07a667bd06c
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2019-11-24 15:07:34 +0100

    Added trim and email filter

--- mail.php
+++ mail.php
@@ -1,11 +1,34 @@
-<?php $name = $_POST['fname'];
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  //Method accepted
+} else {
+  exit('Invalid request!');
+}
+
+$name = $_POST['fname'];
 $email = $_POST['email'];
-$phoe = $_POST['phone'];
+$phone = $_POST['phone'];
 $message = $_POST['message'];
-$formcontent = "From: $name \n Message : $message";
+$formcontent = "From: $name \n Phone : $phone \n Email: $email \n Message : $message";
 $recipient = "lucian@murmurache.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
+
+if(!empty($_POST['fname']) && !empty($_POST['email']) && !empty(['message'])) {
+
+  $fname = trim(htmlspecialchars($_POST['fname']));
+
+  $email = trim(htmlspecialchars($_POST['email']));
+  $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+  if ($email === false) {
+    exit('Invalid email');
+  }
+
+  $phone = trim(htmlspecialchars($_POST['phone']));
+
+  $message = trim(htmlspecialchars($_POST['message']));
+}
+
 mail($recipient, $subject, $formcontent, $mailheader) or die("An error occurred, try again!");
-echo "Thank you for your message!";
+echo "Thank you for your message, I will get back to you shortly!";
 ?>
